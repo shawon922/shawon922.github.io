61f37902f14f872bcfe3a252a7e33c9170bf024c
Md Mahfuzur Rahman <shawon922@gmail.com>
2019-03-14 20:31:50 +0600

    video chat added

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+let localStream = null;
+let peer = null;
+let existingCall = null;
+
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+  .then(function (stream) {
+    console.log("User media stream: ", stream);
+  })
+  .catch(function (err) {
+    console.log("User media error: ", err);
+  });
+
+peer = new Peer({
+  key: '4d6c4179-3ee7-4667-a8db-199cffa91647',
+  debug: 3
+});
+
+peer.on('open', function () {
+  $('#my-id').text(peer.id);
+});
+
+peer.on('error', function (err) {
+  alert(err.message);
+});
+
+peer.on('close', function () {
+});
+
+peer.on('disconnected', function () {
+});
+
+
